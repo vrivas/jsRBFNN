966e096e6532edb47052e8bcedfd7f47537b10a1
Victor M. Rivas Santos <vrivas@ujaen.es>
2015-07-30 00:24:38 +0200

    Moving the function to computer euclidean distance

--- RBFNeuron.js
+++ RBFNeuron.js
@@ -20,38 +20,12 @@ try {
         this.radius = _radius || null;
 
         /**
-         * Computes eculidean distance between two arrays
-         * @param {array of floats} a First array
-         * @param {array of floats} b Second array
-         * @throws {RangeError} Error if lengths of a and b are differents
-         * @returns {Number} The euclidean distance
-         */
-        this.distance = function (a, b) {
-            if (a.length != b.length)
-                throw new RangeError("Distance can't be computed: "
-                        + "points have different lengths; "
-                        + a.length + " vs " + b.length);
-            return Math.sqrt(
-                    a.map(function (e, i) {
-                        return e - b[i];
-                    })
-                    .map(function (e) {
-                        return Math.pow(e, 2);
-                    })
-                    .reduce(function (a, b) {
-                        return a + b;
-                    }, 0)
-                    );
-        }
-
-
-        /**
          * Obtanis an output given an entry point using a Gaussian activation function
          * @param {array of floats} _point
          * @returns {Number} The neuron¡s output
          */
         this.apply = function (_point) {
-            return Math.exp(-Math.pow(this.distance(this.center, _point), 2) / (2 * Math.pow(this.radius, 2)));
+            return Math.exp(-Math.pow(js_rbfnn.distance(this.center, _point), 2) / (2 * Math.pow(this.radius, 2)));
         }
 
 
